9ec5fdec8a4249bef167f905a1dba2827c612997
Temetiu <>
2023-08-02 16:42:09 +0200

    Placeholder and printing values

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use std::io;
 
 fn main() {
+    let mut player_number = String::new(); // mut to set the variable 'player_number' mutable (immutable by default)
+    let x = 5;
+    let y = 10;
+
     println!("Hello World!");
     println!("Mystery number game");
     println!("===================");
     println!();
 
     println!("Enter a number: ");
-    let mut player_number = String::new();
-
+    
     io::stdin()
         .read_line(&mut player_number)
         .expect("Failed to read number");
     
-    println!("You entered {player_number}");
+    println!("You entered {}", player_number);
+
+    println!("x = {}, y = {}", x, y);
+    println!("Then x + y = {} + {}", x, y);
+    println!("           = {}", x+y);
 }
+
