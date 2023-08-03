913e9d9c6b90da28ca82ce57f68c30c2ab9147eb
Temetiu <>
2023-08-03 16:43:22 +0200

    Adding loop at the guess number step

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,30 @@ fn main() {
     println!("Mystery number game");
     println!("===================");
     println!();
+    loop {
+        println!("Enter a number: ");
+        let mut player_number = String::new(); // mut to set the variable 'player_number' mutable (immutable by default)
 
-    println!("Enter a number: ");
-    let mut player_number = String::new(); // mut to set the variable 'player_number' mutable (immutable by default)
+        io::stdin()
+            .read_line(&mut player_number)
+            .expect("Failed to read number");
 
-    io::stdin()
-        .read_line(&mut player_number)
-        .expect("Failed to read number");
+        // player_number is a string type
+        // we need to convert it to unsigned 32-bit number type
+        // trim() nethod used to remove whitespaces at the beginning and end of player_number, but also \n for new line
+        let player_number: u32 = player_number.trim().parse().expect("Please enter a number");
 
-    // player_number is a string type
-    // we need to convert it to unsigned 32-bit number type
-    // trim() nethod used to remove whitespaces at the beginning and end of player_number, but also \n for new line
-    let player_number: u32 = player_number.trim().parse().expect("Please enter a number");
+        println!("You entered {}", player_number);
 
-    println!("You entered {}", player_number);
-
-    match player_number.cmp(&secret_number) {
-        Ordering::Less => println!("Your guess is too small"),
-        Ordering::Equal => println!("Congratulation, your guess is right!"),
-        Ordering::Greater => println!("Your guess is too big"),
+        match player_number.cmp(&secret_number) {
+            Ordering::Less => println!("Your guess is too small"),
+            Ordering::Equal => {
+                println!("Congratulation, your guess is right!");
+                break;
+            }
+            Ordering::Greater => println!("Your guess is too big"),
+        }
     }
-
     println!("The secret number was {} !", secret_number);
     println!();
 }
