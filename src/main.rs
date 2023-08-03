c561e934db7d4576e5e83419e6c050390b0aaed0
Temetiu <>
2023-08-03 16:08:41 +0200

    Convert player_number from string to u32 type

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     // Generate a random number in [1;100]
@@ -16,7 +17,19 @@ fn main() {
     io::stdin()
         .read_line(&mut player_number)
         .expect("Failed to read number");
+
+    // player_number is a string type
+    // we need to convert it to unsigned 32-bit number type
+    // trim() nethod used to remove whitespaces at the beginning and end of player_number, but also \n for new line
+    let player_number: u32 = player_number.trim().parse().expect("Please enter a number");
+
     println!("You entered {}", player_number);
+
+    match player_number.cmp(&secret_number) {
+        Ordering::Less => println!("Your guess is too small"),
+        Ordering::Equal => println!("Congratulation, your guess is right!"),
+        Ordering::Greater => println!("Your guess is too big"),
+    }
 
     println!("The secret number was {} !", secret_number);
     println!();
